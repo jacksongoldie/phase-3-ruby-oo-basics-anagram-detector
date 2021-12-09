d17e69d6e704b38cf0bfe71e34af3eeca6e685b2
Goldie <goldiejackson25@yahoo.com>
2021-12-09 09:45:53 -0600

    commit tree 9b1911bfe1e9d848b5279417aa42ff19f8990306

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word 
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        matches = []
+
+       word_letters =  @word.chars
+       word_sorted = word_letters.sort
+
+       #binding.pry
+        array.map {|x| 
+            letters_array = x.chars
+            sorted_array = letters_array.sort
+        
+            if sorted_array == word_sorted
+                matches << x
+            end
+        }
+        matches
+    end
+end
